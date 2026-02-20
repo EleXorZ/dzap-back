cfc3c9594779268bd483c5e678abc21e80b7e738
EleXorZ <dilyskevin@live.fr>
2026-02-20 14:54:34 +0100

    Init project

---
+++ src/main/java/dzap/DzapApplication.java
@@ -0,0 +1,13 @@
+package dzap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DzapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DzapApplication.class, args);
+	}
+
+}
